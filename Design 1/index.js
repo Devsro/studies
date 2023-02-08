b300ba1fa486ce5608fcfa5042143f7e3b1e4e6b
Năstase Marius Daniel <mdnastase1@gmail.com>
2023-02-09 01:50:30 +0200

    DRY
    dont need the first variables unless you plan on doing something with them later

--- Design 1/index.js
+++ Design 1/index.js
@@ -1,55 +1,23 @@
-
-
+var imageCards = [];
 // Changes the images into gifs once hovered
 let imageCard1 = document.getElementById("Project1");
 let imageCard2 = document.getElementById("project2");
 let imageCard3 = document.getElementById("project3");
 let imageCard4 = document.getElementById("project4");
+imageCards.push(document.getElementById("Project1"));
+imageCards.push(document.getElementById("project2"));
+imageCards.push(document.getElementById("project3"));
+imageCards.push(document.getElementById("project4"));
 
-
-imageCard1.addEventListener('mouseover', () => {
-    imageCard1.src = "assets/Project1.gif";
-    imageCard1.classList = "fade-in-image";
-})
-
-imageCard1.addEventListener('mouseleave', () => {
-    imageCard1.src = "assets/Project1.png";
-    imageCard1.classList = "";
-})
-
-imageCard2.addEventListener('mouseover', () => {
-    imageCard2.src = "assets/";
-})
-
-imageCard2.addEventListener('mouseleave', () => {
-    imageCard2.src = "assets/";
-})
-
-imageCard3.addEventListener('mouseover', () => {
-    imageCard3.src = "assets/";
-})
-
-imageCard3.addEventListener('mouseleave', () => {
-    imageCard3.src = "assets/";
-})
-
-imageCard4.addEventListener('mouseover', () => {
-    imageCard4.src = "assets/";
-})
-
-imageCard4.addEventListener('mouseleave', () => {
-    imageCard4.src = "assets/";
-})
-
-const imageCards = [imageCard1, imageCard2, imageCard3, imageCard4];
-
-imageCards.forEach(addEventListener('mouseover'), () => {
-    classList = "fade-in-image";
-});
-imageCards.forEach(addEventListener('mouseleave'), () => {
-    classList = "";
-});
-
-function changeToGif(imageCard){
-    imageCard.src = imageCard.id += ".png";
+for(let i=0; i<imageCards.length;i++){
+    imageCards[i].addEventListener('mouseover', () => {
+        let id = imageCards[i].getAttribute("id");
+        imageCards[i].src = 'assets/' + id + '.gif';
+        imageCards[i].classList = "fade-in-image";
+    });
+    imageCards[i].addEventListener('mouseleave', () => {
+        let id = imageCards[i].getAttribute("id");
+        imageCards[i].src = 'assets/' + id + '.png';
+        imageCards[i].classList = "fade-in-image";
+      });
 }
