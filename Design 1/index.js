f9add54806c4138d57fd5ae7c0e6e0143ded8efb
Năstase Marius Daniel <mdnastase1@gmail.com>
2023-02-09 02:10:56 +0200

    dont have to manually add projects to array now

--- Design 1/index.js
+++ Design 1/index.js
@@ -1,23 +1,15 @@
-var imageCards = [];
 // Changes the images into gifs once hovered
-let imageCard1 = document.getElementById("Project1");
-let imageCard2 = document.getElementById("project2");
-let imageCard3 = document.getElementById("project3");
-let imageCard4 = document.getElementById("project4");
-imageCards.push(document.getElementById("Project1"));
-imageCards.push(document.getElementById("project2"));
-imageCards.push(document.getElementById("project3"));
-imageCards.push(document.getElementById("project4"));
+var imageCards = document.getElementsByClassName("project");
 
 for(let i=0; i<imageCards.length;i++){
     imageCards[i].addEventListener('mouseover', () => {
         let id = imageCards[i].getAttribute("id");
-        imageCards[i].src = 'assets/' + id + '.gif';
-        imageCards[i].classList = "fade-in-image";
+        imageCards[i].src = 'assets/' + id +'.gif';
+        imageCards[i].classList.add("fade-in-image");
     });
     imageCards[i].addEventListener('mouseleave', () => {
         let id = imageCards[i].getAttribute("id");
         imageCards[i].src = 'assets/' + id + '.png';
-        imageCards[i].classList = "fade-in-image";
+        imageCards[i].classList.remove("fade-in-image");
     });
 }
